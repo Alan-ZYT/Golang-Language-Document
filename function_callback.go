6f72db4054c0949624ff2edf1ad7d733bfc44783
Alan <41845419+Alan-ZYT@users.noreply.github.com>
2019-06-25 17:15:21 +0800

    function_callback
    function_callback

---
+++ function_callback.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		高阶函数：
+			根据go语言的数据类型的特点，可以将一个函数作为另一个函数的参数。
+
+		fun1(),fun2()
+		将fun1函数作为了fun2这个函数的参数。
+
+				fun2函数：就叫高阶函数
+					接收了一个函数作为参数的函数，高阶函数
+
+				fun1函数：回调函数
+					作为另一个函数的参数的函数，叫做回调函数。
+	*/
+
+	//设计一个函数，用于求两个整数的加减乘除运算
+	fmt.Printf("%T\n", add)       //func(int, int) int
+	fmt.Printf("%T\n", operation) //func(int, int, func(int, int) int) int
+
+	res1 := add(1, 2)
+	fmt.Println(res1)
+
+	res2 := operation(10, 20, add)
+	fmt.Println(res2)
+
+	res3 := operation(5, 2, sub)
+	fmt.Println(res3)
+
+	fun1 := func(a, b int) int {
+		return a * b
+	}
+
+	res4 := operation(10, 4, fun1)
+	fmt.Println(res4)
+
+	res5 := operation(100, 8, func(a, b int) int {
+		if b == 0 {
+			fmt.Println("除数不能为零")
+			return 0
+		}
+		return a / b
+	})
+	fmt.Println(res5)
+}
+
+//操作函数
+func operation(a, b int, fun func(int, int) int) int {
+	fmt.Println(a, b, fun) //打印 3个参数
+	res := fun(a, b)
+	return res
+}
+
+//加法运算
+func add(a, b int) int {
+	return a + b
+}
+
+//减法运算
+func sub(a, b int) int {
+	return a - b
+}
