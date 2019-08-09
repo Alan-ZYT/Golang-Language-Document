94f4dadf13239c3e50bfc855223aff4dac5ed5b1
Alan <41845419+Alan-ZYT@users.noreply.github.com>
2019-08-09 14:44:27 +0800

    Update map_sort.go

--- map_sort.go
+++ map_sort.go
@@ -1,5 +1,5 @@
-//map排序：
-//先获取所有key，把key进行排序，再按照排序好的key，进行遍历。
+// map排序：
+// 先获取所有key，把key进行排序，再按照排序好的key，进行遍历。
 package main
 
 import (
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	
+	// 方法1
 	m := map[string]string{"a": "1", "b": "2", "c": "3", "d": "4", "e": "5"}
 
 	var slice []string
@@ -20,5 +22,18 @@ func main() {
 
 	for _, v := range slice {
 		fmt.Println(m[v])
+	}
+	
+	// 方法2
+	m1 := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	s := make([]int, len(m1))
+	i := 0
+	for k := range m1 {
+		s[i] = k
+		i++
+	}
+	sort.Ints(s)
+	for _, v := range s {
+		fmt.Println(m1[v])
 	}
 }
